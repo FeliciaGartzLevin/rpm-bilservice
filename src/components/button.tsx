e57c71af8e0ee3b611c80889538588377978b79b
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2025-05-15 22:06:25 +0200

    created a button component

---
+++ src/components/button.tsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx';
+import React from 'react';
+
+type Props = {
+	purpose: 'accent' | 'normal';
+	onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+	text: string;
+};
+
+export const Button: React.FC<Props> = ({ purpose, onClick, text }) => {
+	return (
+		<button
+			className={clsx(
+				'mt-6 px-7 py-3 rounded-xl font-header text-2xl',
+				purpose === 'accent'
+					? 'bg-accentColor-strong'
+					: 'bg-accentColor-light',
+			)}
+			onClick={onClick}
+			type="button"
+		>
+			{text}
+		</button>
+	);
+};
